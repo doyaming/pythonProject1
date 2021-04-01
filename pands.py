41005c8220b8fd535a729dc5fbeb88a78f8bbb40
t12652 <mdki150625@gmail.com>
2021-04-01 10:45:53 +0800

    AIDB

--- pands.py
+++ pands.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv('/home/t12652/dirtydata.csv')
 
 
-df["Calories"].fillna(125, inplace = True)
+df["Calories"].fillna(140, inplace = True)
 
 
 print(df.to_string())
